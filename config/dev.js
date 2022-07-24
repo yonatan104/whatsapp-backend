b6f7bae87f6fa4b0290b3208e1faaa2235ab30ca
yonatan104 <yonatangeller@gmail.com>
2022-07-24 12:18:36 +0300

    v2

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  dbURL:
+    'mongodb+srv://yonatan104:0544648070@cluster0.gxpwljv.mongodb.net/?retryWrites=true&w=majority',
+}
---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'dbURL': 'mongodb+srv://yonatan104:0544648070@cluster0.gxpwljv.mongodb.net/?retryWrites=true&w=majority',
+}
